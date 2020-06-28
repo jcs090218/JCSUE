46db9c8d0a7074991cdd7449742befbec542cd25
jenchieh <jcs090218@gmail.com>
2020-06-28 23:01:51 +0800

    Added new component events => order event.

---
+++ Plugins/JCSUE_Plugin/Source/JCSUE_Plugin/Components/Events/JCS_OrderEvent.h
@@ -0,0 +1,66 @@
+/**
+ * $File: JCS_OrderEvent.h $
+ * $Date: 2020-06-28 16:48:08 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2020 by Shen, Jen-Chieh $
+ */
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "JCS_OrderEvent.generated.h"
+
+/**
+ * @class UJCS_OrderEvent
+ * @brief Execute an operation in order with interval of time.
+ */
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class JCSUE_PLUGIN_API UJCS_OrderEvent : public UActorComponent
+{
+public:
+    TFunction<void()> execution;
+
+    UPROPERTY(EditAnywhere,
+        Category = "Runtime Variables",
+        meta = (ToolTip = "Time for each execution."))
+    float intervalTime = 0.0f;
+
+private:
+    GENERATED_BODY()
+
+    // Check if there are event going to execute.
+    bool m_active = false;
+
+    float m_timer = 0.0f;
+
+public:
+    UJCS_OrderEvent();
+
+    virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+    /**
+     * @func StartEvent
+     * @brief Start a new event with interval of time and execution orders.
+     */
+    void StartEvent(float it, TFunction<void()> fnc);
+
+    /**
+     * @func DoneEvent
+     * @brief Terminate the current event loop.
+     */
+    void DoneEvent();
+
+    /* operator */
+
+    /* setter */
+
+    /* getter */
+
+private:
+    /**
+     * @func DoLoop
+     * @brief Loop through the execution.
+     */
+    void DoLoop(float DeltaTime);
+};
--- Plugins/JCSUE_Plugin/Source/JCSUE_Plugin/JCSUE_Plugin.h
+++ Plugins/JCSUE_Plugin/Source/JCSUE_Plugin/JCSUE_Plugin.h
@@ -35,22 +35,28 @@ public:
 #define JCSUE_RELEASE
 #endif
 
- // defines
+/* Defines */
 #include "JCS_StdAfx.h"
 #include "JCS_Name.h"
 
-// enums
+/* Enums */
 #include "Core/Enums/JCS_Axis.h"
 #include "Core/Enums/JCS_Keys.h"
 
-// supoort
+/* Supoort */
 #include "Supports/JCS_Debug.h"
 #include "Supports/JCS_Utility.h"
 #include "Supports/JCS_Input.h"
 
-// core
+/* Core */
 #include "Core/JCS_Mathf.h"
 #include "Core/JCS_String.h"
 #include "Core/JCS_WString.h"
 #include "Core/Tweener/JCS_Tweener.h"
 #include "Core/Tweener/JCS_Easing.h"
+
+/* Components */
+#include "Components/JCS_Character.h"
+#include "Components/Actions/JCS_GoStraightAction.h"
+#include "Components/Actions/JCS_RotateAction.h"
+#include "Components/Events/JCS_OrderEvent.h"
